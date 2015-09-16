1033410a8d70e0efb919b5ad03c2bad9219ffc87
Samuel Sadok <samuel.sadok@bluewin.ch>
2015-09-16 22:54:25 +0200

    temporarily removed shared-c

--- shared-c/arch/avr/eeprom.c
+++
@@ -1,9 +0,0 @@
-/*
-*
-* Provides raw read/write access to the built-in EEPROM.
-* Do not use this driver directly, use the functions in system/nvm.c instead.
-*
-* created: 14.03.15
-*
-*/
-
--- shared-c/arch/csr/nvm_raw.c
+++
@@ -1,9 +0,0 @@
-/*
-*
-* Provides raw access to the on-board non-volatile memory.
-* This may either be an SPI flash or an I2C EEPROM.
-*
-* created: 13.03.15
-*
-*/
-
